f98f565b6b09b413af5a496d4ccf2282e69c7181
waseemwasi1 <91869822+waseemwasi1@users.noreply.github.com>
2025-03-17 19:23:24 +0530

    updated

--- Day-2/06-variables-implementation.tf
+++ Day-2/06-variables-implementation.tf
@@ -1,6 +1,5 @@
 # Variables Demo
 
-```hcl
 
 # Define an input variable for the EC2 instance type
 variable "instance_type" {
@@ -31,5 +30,3 @@ output "public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.example_instance.public_ip
 }
-
-```
